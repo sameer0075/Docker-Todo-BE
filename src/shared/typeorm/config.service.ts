19e7c96203a03cb2ece423573acf818f26d90b0f
sameer0075 <sameerbutt151@gmail.com>
2024-09-04 23:45:36 +1100

    add database uri

--- src/shared/typeorm/config.service.ts
+++ src/shared/typeorm/config.service.ts
@@ -5,7 +5,7 @@ dotenv.config();
 console.log("process.env.DATABASE_UR", process.env.DATABASE_UR)
 const databaseConfig: DataSourceOptions = {
   type: 'postgres',
-  url: process.env.DATABASE_URL,
+  url: "postgres://postgres:Superman1234@database-1.c7g8c84kuof9.us-east-1.rds.amazonaws.com:5432/todo-app",
   entities: ['dist/**/*.entity{.js,.ts}'],
   synchronize: false,
   migrations: ['dist/database/*.{ts,js}'],
